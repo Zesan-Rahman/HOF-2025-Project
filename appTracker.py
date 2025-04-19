5afb735973e0ebaaa558384806b373680b1f8c99
Zesan-Rahman <76439671+Zesan-Rahman@users.noreply.github.com>
2025-04-18 22:49:53 -0400

    Add files via upload

---
+++ appTracker.py
@@ -0,0 +1,53 @@
+import time
+import pygetwindow as gw
+import socket
+
+
+# Initialize variables
+SERVERPORT = 65432; #SET SERVER PORT NUMBER HERE
+current_window = None
+start_time = None
+app_data = {} #Dictionary that stores app name and amount of time spent on it
+
+def track_app_time():
+    global current_window, start_time
+    HOST = "127.0.0.1"  #localhost
+    PORT = SERVERPORT
+    sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockfd.connect((HOST, PORT))
+
+    while True:
+        new_window = gw.getActiveWindow()
+
+        if new_window != current_window:
+            # Calculate the time spent on the previous window
+            if current_window is not None:
+                end_time = time.time()
+                elapsed_time = end_time - start_time
+                app_name = current_window.title
+
+                if app_name in app_data:
+                    app_data[app_name] += elapsed_time
+                else:
+                    app_data[app_name] = elapsed_time
+
+                # Save the data to TrackRecords.txt
+                with open("TrackRecords.txt", "a") as file:
+                    current_datetime = time.strftime("%Y-%m-%d %H:%M:%S")
+                    file.write(f"{current_datetime} - Time spent on {app_name}: {elapsed_time:.2f} seconds\n")
+                    data = f"{app_name}: {elapsed_time:.2f} seconds\n"
+                    sockfd.sendall(data.encode())
+
+            # Update current window and start time
+            current_window = new_window
+            start_time = time.time()
+
+        time.sleep(1)  # Check the active window every 1 second
+
+if __name__ == "__main__":
+    try:
+        track_app_time()
+    except KeyboardInterrupt:
+        # Print the final app usage data when the script is terminated
+        for app, time_spent in app_data.items():
+            print(f"Total time spent on {app}: {time_spent:.2f} seconds")
